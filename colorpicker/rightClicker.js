c06a1f3ba652615d661b22a6f243e638ce1eb3c7
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-21 20:01:25 +0200

    Ending my boy :'(

--- colorpicker/rightClicker.js
+++ colorpicker/rightClicker.js
@@ -1,3 +1,21 @@
 /**
  * Created by Rémy Kaloustian on 21/04/2016.
  */
+$( document ).ready(function() {
+    $('.color_div').click(function(event)
+    {
+        var the_id = $(this).attr("href"); //On localise la cible de l'ancre
+
+        $('html, body').animate( //--> On anime en faisant un scroll slow jusqu'? la cible
+            {
+                scrollTop:$(the_id).offset().top
+            }, 'slow');
+        //var str = $("#selected").data('internalid')
+
+        $("#selected").html("Selected color : " + $(this).attr("data-internalid").toString());
+
+        return false;
+    });
+
+
+});
