20a969c282dca8617a130e863fa098c344fcbe7b
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-24 13:34:14 +0200

    fix #14

--- flatdesign/animationScript.js
+++ flatdesign/animationScript.js
@@ -2,26 +2,29 @@
  * Created by Rémy Kaloustian on 22/04/2016.
  */
 $(document).ready(function() {
-    $(".animsition").animsition({
-        inClass: 'fade-in',
-        outClass: 'fade-out',
-        inDuration: 1500,
-        outDuration: 800,
-        linkElement: '.animsition-link',
-        // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
-        loading: true,
-        loadingParentElement: 'body', //animsition wrapper element
-        loadingClass: 'animsition-loading',
-        loadingInner: '', // e.g '<img src="loading.svg" />'
-        timeout: false,
-        timeoutCountdown: 5000,
-        onLoadEvent: true,
-        browser: [ 'animation-duration', '-webkit-animation-duration'],
-        // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-        // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-        overlay : false,
-        overlayClass : 'animsition-overlay-slide',
-        overlayParentElement : 'body',
-        transition: function(url){ window.location.href = url; }
+
+    $( window ).resize(function() {
+        if($(window).width() < 1043)
+        {
+            $("#home_icon").css("display", "block");
+            $("#home_icon").css("margin-left", "auto");
+            $("#home_icon").css("margin-right", "auto");
+            console.log("Resizing");
+
+        }
+
+        else if($(window).width() > 1043)
+        {
+            $("#home_icon").css("display", "block");
+            $("#home_icon").css("margin-left", "0");
+            $("#home_icon").css("margin-right", "0");
+            console.log("Resizing");
+
+        }
     });
+
+
+
+
+
 });
