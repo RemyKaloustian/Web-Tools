143a334c46c2feee0b7a823225ab3b4e27f8c493
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-05-13 17:56:51 +0200

    Adding the Fontpicker project to webtools project

---
+++ fontpicker/index.php
@@ -0,0 +1,60 @@
+﻿<?php
+include 'utils.php';
+?>
+
+<?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Font Picker</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="fonts.css">
+        <link rel="stylesheet" type="text/css" href="hover-min.css">
+
+
+</head>
+<body>
+
+    <h1 class="centered" id="title">Font Picker</h1>
+    <img src="boss.png" id="boss" >
+    <h4 class="centered" id="subtitle">Use fonts like a boss.</h4>
+
+    <p class="centered" id="scroll">Scroll down mate.</p>';
+?>
+
+
+<?php
+DisplayContainer("Edmond Sans Regular", "fonts/Edmondsans-Regular.otf", "edmond");
+DisplayContainer("Edmond Sans Bold", "fonts/Edmondsans-Bold.otf", "edmondBold");
+DisplayContainer("Proxima Nova", "fonts/Proxima.otf", "proxima");
+DisplayContainer("Raleway Light", "fonts/Raleway-Light.ttf", "ralewayLight");
+DisplayContainer("Raleway Regular", "fonts/Raleway-Regular.ttf", "ralewayRegular");
+DisplayContainer("Raleway Bold", "fonts/Raleway-Bold.ttf", "ralewayBold");
+DisplayContainer("Open Sans", "fonts/opensans-webfont.ttf", "openSans");
+DisplayContainer("Lato Light", "fonts/Lato-Light_0.ttf", "latoLight");
+DisplayContainer("Lato Regular", "fonts/Lato-Regular.ttf", "latoRegular");
+DisplayContainer("Lato Bold", "fonts/Lato-Bold.ttf", "latoBold");
+DisplayContainer("Aller Regular", "fonts/Aller_Rg.ttf", "allerRegular");
+DisplayContainer("Aller Light", "fonts/Aller_Lt.ttf", "allerLight");
+DisplayContainer("Aller Bold", "fonts/Aller.ttf", "allerBold");
+DisplayContainer("Aleo Regular", "fonts/Aleo-Regular.otf", "aleoRegular");
+DisplayContainer("Aleo Light", "fonts/Aleo-light.otf", "aleoLight");
+DisplayContainer("Aleo Bold", "fonts/Aleo-Bold.otf", "aleoBold");
+DisplayContainer("Bariol", "fonts/Bariol_Regular.otf", "bariol");
+DisplayContainer("Mido", "fonts/Mido.otf", "mido");
+DisplayContainer("Lovelo Black", "fonts/Lovelo-Black.otf", "loveloBlack");
+DisplayContainer("Lovelo Line", "fonts/Lovelo-Line-Light.otf", "loveloLight");
+DisplayContainer("Code Bold", "fonts/CODE-Bold.otf", "codeBold");
+DisplayContainer("Code Light", "fonts/CODE-Light.otf", "codeLight");
+DisplayContainer("Chunkfive", "fonts/Chunkfive.otf", "chunkfive");
+DisplayContainer("Sintony Bold", "fonts/Sintony-Bold.otf", "sintonyBold");
+DisplayContainer("Sintony Regular", "fonts/Sintony-Regular.otf", "sintonyRegular");
+DisplayContainer("Roboto", "fonts/Roboto-Regular.ttf", "roboto");
+DisplayContainer("Varela Round", "fonts/VarelaRound-Regular.ttf", "varela");
+
+?>
+
+
+</body>
+</html>
---
+++ fontpicker/utils.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rémy Kaloustian
+ * Date: 09/04/2016
+ * Time: 08:12
+ */
+
+
+function DisplayContainer($fontName, $fontUrl, $class)
+{
+    echo ' <div class="container ' . $class . '">
+        <h4 class="font_name">Font : ' . $fontName . '</h4>
+        <h1 class="huge_title">This is a huge title</h1>
+        <h3 class="victim_title">This is a victim title </h3>
+        <p class="paragraph">This is a text about titties power. For most people,
+            titties power is not a real thing. They would grant success to other things
+            like luck or  help from other people. However it has been proved that individuals
+            with high titties strength were more likely to solve difficult problems easier than
+            people with weak titties. Indeed, scientists made an experiment using both weak titties
+            and strong titties. The participant had to lift Thomas Monzein\'s charisma with their titties.
+Some would say that this is an impossible thing to accomplish, Thomas Monzein\'s charisma being
+            one of the most powerful charisma on Earth along with Fabien Hermenier and the business angels
+            ones. But the experiment showed that people with powerful titties were able to lift Thomas\' charisma
+            six inches above the ground, whereas people with weak titties couldn\'t even move it and got their titties dislocated.<br>
+
+            A recent survey led in Polytech gave some useful evidence on the impact of titties
+            power on the student consciousness. Some regret they did not use their titties potential before
+            : "I wish I had big powerful titties. My little titties have become a scourge, they follow me even on Stash",
+            said Zineb El Haouari, a camel dealer.
+
+            "Yeeaaaas but, voila quoi , \'fin...,yeaaaas.", replied Antoine Lupiac, a fruit dealer in Peru.
+
+            <br>
+
+            This can only show the importance of  titties in the daily life.
+        </p>
+
+        <a href="' . $fontUrl . '" class="download hvr-sweep-to-right">Download ' . $fontName . '</a>
+
+        <div class="separator"></div>
+    </div>';
+
+}
+
+
+?>
