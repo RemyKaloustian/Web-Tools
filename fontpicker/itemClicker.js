c1e01e2647ee9b86e0b8db79c7c269a141f3af44
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-05-14 21:00:34 +0200

    New fontpicker working

--- fontpicker/itemClicker.js
+++ fontpicker/itemClicker.js
@@ -6,12 +6,26 @@ $(document).ready(function () {
     //$(".color_div").css("color",$(this).attr("data-internalid").toString())
 
     $('.font_item').click(function (event) {
-        var fontName = $(this).attr("data-font").toString()
+        var fontName = $(this).attr("data-font").toString();
+
+        // var downloadName = $(this).attr("data-load").toString();
+
+        $("#content_div").fadeOut("slow", function () {
+            $("#huge_title").css("fontFamily", fontName);
+            $("#victim_title").css("fontFamily", fontName);
+            $("#text_title").css("fontFamily", fontName);
+
+            $("#download").css("fontFamily", fontName);
+            $("#download").text("Download " + $(event.target).text());
+            $("#download").attr("href", "fonts/" + $(event.target).attr("data-load").toString())
 
 
-        $("#huge_title").css("fontFamily", fontName);
-        $("#victim_title").css("fontFamily", fontName);
-        $("#text_title").css("fontFamily", fontName);
+        });
+
+        $("#content_div").fadeIn("slow", function () {
+
+
+        });
 
 
         return false;
