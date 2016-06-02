06cb06664cf253f8aae56ea53c7cd173066de7b1
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-02 18:06:11 +0200

    fix #46, fix #47, fix #21

--- colorpicker/toClipboard.js
+++ colorpicker/toClipboard.js
@@ -11,6 +11,13 @@ $(document).ready(function () {
         "#mauve", "#sienna", "#grey", "#blue_grey", "#light_grey", "#dark_grey", "#thin_grey", "#solid_grey",
         "#dat_violet", "#midnight_blue", "#soft_orange", "#nice_green",
 
+
+        "#red2", "#pink2", "#purple", "#deep_purple", "#indigo2", "#blue2", "#light_blue", "#cyan2",
+        "#teal2", "#green2", "#light_green", "#lime2", "#yellow2", "#amber2", "#orange2", "#deep_orange",
+        "#brown2", "#grey2", "#dark_brown", "#strong_pink", "#soft_pink", "#not_so_turquoise", "#bold_red", "#sad_purple",
+        "#beige", "#sad_green", "#shadow_grey", "#almost_strong_pink", "#flatest_violet", "#sad_orange", "#happy_blue",
+        "#orose", "#sad_blue", "#nature_green", "#sky_blue", "#super_happy_blue"
+
     ];
 
     colors.forEach(function (item) {
