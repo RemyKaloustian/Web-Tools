fe212f65772d7e8e424b8a3a03456ff3664b3d5e
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-03 16:39:02 +0200

    fix #26 fix #54   :rage3:  :rage3:  :rage3:  :rage3:  :rage3:

---
+++ fontpicker/IEsucks.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Rémy Kaloustian on 03/06/2016.
+ */
+
+
+
+//Is used to warn the user that Internet Explorer needs thousands of CSS lines to make a font work ;)
+
+function msieversion()//"This website is not designed to work with Internet Explorer. We recommend using an other browser"
+{
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        alert("This website is not designed to work with Internet Explorer. We recommend using an other browser.");
+    }
+    else  // If another browser, return 0
+    {
+
+    }
+
+    return false;
+}
+
+
+$(document).ready(function () {
+
+
+   msieversion();
+
+});
